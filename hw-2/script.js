a39c5f04c136b69726024db5a3180cc7c429abaf
NikolasLinden <65183525+NikolasLinden@users.noreply.github.com>
2021-01-24 22:28:36 -0700

    Add files via upload

---
+++ hw-2/script.js
@@ -0,0 +1,3 @@
+console.log("My three favorite websites are Youtube, Roosterteeth, and Crunchyroll");
+console.log("My three favorite games are Blue Dragon, Bravely Default, and Xenoblade Chronicles X");
+console.log("The three artists I prefer are The Rolling Stones, Linked Horizon, and The Who");
