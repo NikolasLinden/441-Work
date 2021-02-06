ab400b56561cad09c99bf990f6777dc81fbaeceb
NikolasLinden <65183525+NikolasLinden@users.noreply.github.com>
2021-02-05 20:26:52 -0700

    Add files via upload

--- hw-4/script.js
+++ hw-4/script.js
@@ -1,31 +1,31 @@
-function Quest1()
+function quest1()
       {
-        var qeustQuestion = document.getElementById('question1'.value)
-        var questDecision = document.getElementById('decision1')
-        if (answer1 === "Yes")
+        var questquestion = document.getElementById('question1'.value)
+        var questdecision = document.getElementById('decision1')
+        if (questquestion === "Yes")
         { document.getElementById('question1');
         document.getElementById('questbutton1');
         document.getElementById("question2");
         document.getElementById("questbutton2");
-        Quest.innerHTML = "So you want to live dangerously? Okay then, beef or fish tacos?"
+        quest.innerHTML = "So you want to live dangerously? Okay then, beef or fish tacos?"
 
       }
-      else if (answer1 === "No") {
-        Quest.innerHTML = "Looks like there's nothing else to eat! You'll have to deal with it >:)" + "Beef or fish Tacos?"
+      else if (questquestion === "No") {
+        quest.innerHTML = "Looks like there's nothing else to eat! You'll have to deal with it >:)" + "Beef or fish Tacos?"
         }
-        else { Quest.innerHTML = "Invalid answer";
+        else { quest.innerHTML = "Invalid answer";
 
 
-       Quest.innerHTML = "That is an invalid answer. Try again";
+       quest.innerHTML = "That is an invalid answer. Try again";
       }
       }
-      function Quest2()
+      function quest2()
       {var topping = document.getElementById('question2'.value);
-      var choice = document.getElementById('questbutton2')
-      if (topping === "Cheese") { Quest.innerHTML = "A solid choice"
+      var questdecision = document.getElementById('decison1')
+      if (topping === "Cheese") { quest.innerHTML = "A solid choice"
 
       }
-      else if (topping === "Salsa") { Quest.innerHTML = "A spicy decision"
+      else if (topping === "Salsa") { quest.innerHTML = "A spicy decision"
 
       }
 
