18c4d1ba64dba7afa31cf5b833a2361c8130c941
NikolasLinden <65183525+NikolasLinden@users.noreply.github.com>
2021-02-13 15:33:09 -0700

    Add files via upload

--- hw-5/script.js
+++ hw-5/script.js
@@ -17,7 +17,7 @@ function gamearray()
   var gameimages = ["images/empire.jpg", "images/everest.jpg", "images/needle.jpg", "images/ranier.jpg", "images/rushmore.jpg"]
   var start = [0,0,0,0,0,0,0,0,0,0];
 while (images.length < 10) {
-  var randomfunction = Math.floor(Math.random()* gameimages.length)
+  var randomfunction = Math.floor(Math.random() * gameimages.length)
   if(start[randomfunction] < 5) {
     images.push(gameimages[randomfunction]);
     start[randomfunction] = start[randomfunction] + 1;
