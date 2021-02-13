774afb3e09983aa1c6b6542fd334fd3b372cafdf
NikolasLinden <65183525+NikolasLinden@users.noreply.github.com>
2021-02-13 15:27:55 -0700

    Add files via upload

--- hw-5/script.js
+++ hw-5/script.js
@@ -2,10 +2,10 @@ var imagecall = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 var blankcall = "images/blank.jpg";
 var images = new Array();
 
-function blankarray();
+function blankarray()
 {
   gamearray();
-  for(bar i = 0; i < imagecall.length; i++)
+  for(var i = 0; i < imagecall.length; i++)
   {
     document.getElementById(imagecall[i]).src= blankcall; }
 }
@@ -18,7 +18,7 @@ function gamearray()
   var start = [0,0,0,0,0,0,0,0,0,0];
 while (images.length < 10) {
   var randomfunction = Math.floor(Math.random()* gameimages.length)
-  if(count[randomfunction] < 5) {
+  if(start[randomfunction] < 5) {
     images.push(gameimages[randomfunction]);
     start[randomfunction] = start[randomfunction] + 1;
   }
