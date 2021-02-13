1c93b9944a4fe5bc6653362308a2d4b7e0d9d845
NikolasLinden <65183525+NikolasLinden@users.noreply.github.com>
2021-02-13 12:42:27 -0700

    Add files via upload

---
+++ hw-5/script.js
@@ -0,0 +1,31 @@
+var imagecall = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
+var blankcall = "images/blank.jpg";
+var images = new Array();
+
+function blankarray();
+{
+  gamearray();
+  for(bar i = 0; i < imagecall.length; i++)
+  {
+    document.getElementById(imagecall[i]).src= blankcall; }
+}
+
+
+
+function gamearray()
+{
+  var gameimages = ["images/empire.jpg", "images/everest.jpg", "images/needle.jpg", "images/ranier.jpg", "images/rushmore.jpg"]
+  var start = [0,0,0,0,0,0,0,0,0,0];
+while (images.length < 10) {
+  var randomfunction = Math.floor(Math.random()* gameimages.length)
+  if(count[randomfunction] < 5) {
+    images.push(gameimages[randomfunction]);
+    start[randomfunction] = start[randomfunction] + 1;
+  }
+}
+}
+
+function click(game)
+{
+  document.getElementById(imagecall[game]).src= images[game];
+}
