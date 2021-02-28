fa5c196eaa3903b8124c00678d40f281a2f39122
NikolasLinden <65183525+NikolasLinden@users.noreply.github.com>
2021-02-27 19:39:51 -0700

    Add files via upload

--- hw-7/script.js
+++ hw-7/script.js
@@ -35,10 +35,10 @@ class imagetime
 function arraytime()
 {
   var hwseven = new imagetime("A picture of the Fremont Bridge");
-  var hwseven1 = new imagetime("insert image here")
-  var hwseven2 = new imagetime("The Fremont Bridge is one of many bridges connecting north seattle to the rest of it, crossing the ship canal just like the Aurora Bridge and Ballard Bridge nearby.")
-  var hwseven3 = new imagetime("Taken by Nikolas A. Linden")
-  var hwseven4 = new imagetime("Taken 2019")
+  var hwseven1 = new imagetime("insert image here");
+  var hwseven2 = new imagetime("The Fremont Bridge is one of many bridges connecting north seattle to the rest of it, crossing the ship canal just like the Aurora Bridge and Ballard Bridge nearby.");
+  var hwseven3 = new imagetime("Taken by Nikolas A. Linden");
+  var hwseven4 = new imagetime("Taken 2019");
   hw7Array.push(hwseven);
   hw7Array.push(hwseven1);
   hw7Array.push(hwseven2);
@@ -49,5 +49,9 @@ function arraytime()
 }
 function accessInformation()
 {
-  document.getElementById("title").innerHTML = hw7Array[1].toString();
+  document.getElementById("title").innerHTML = hw7Array[0].toString();
+  document.getElementById("image").innerHTML = hw7Array[1].toString();
+  document.getElementById("description").innerHTML = hw7Array[2].toString();
+  document.getElementById("author").innerHTML = hw7Array[3].toString();
+  document.getElementById("imageyear").innerHTML = hw7Array[4].toString();
 }
