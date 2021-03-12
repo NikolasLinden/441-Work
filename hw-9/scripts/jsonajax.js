a514618a0a598b926d090a1c100f929aea12ddb7
NikolasLinden <65183525+NikolasLinden@users.noreply.github.com>
2021-03-12 14:23:45 -0700

    Add files via upload

--- hw-9/scripts/jsonajax.js
+++ hw-9/scripts/jsonajax.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
            $("button").click(function () {
                $("#PokemonInfo").load("data/pokedata.json", function(responseText){
-                   var  = JSON.parse(responseText);
+                   var pokemon = JSON.parse(responseText);
                    $("#PokemonInfo").html("Id: " + pokemon.id + "<br>num: " + pokemon.num + "<br>name: " + pokemon.name + "<br>img" + pokemon.image + "<br>type: " + pokemon.type);
                });
            });
