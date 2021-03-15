1a83d839ffa130622c3dbc12658ddf30d1623d9f
NikolasLinden <65183525+NikolasLinden@users.noreply.github.com>
2021-03-15 15:05:56 -0600

    Add files via upload

--- hw-9/scripts/jsonajax.js
+++ hw-9/scripts/jsonajax.js
@@ -1,10 +1,18 @@
-$(document).ready(function () {
-           $("button").click(function () {
-             $.getJSON( "data/pokedex.json", function( data ) {
 
-                console.log(data.pokemon[0].img);
-                console.log(data.pokemon[1].img);
-               $("#PokemonInfo").html("<img src='" + data.pokemon[0].img + "'>" + "<br>" + pokemon.id + "<br>" + pokemon.num + "<br>" + pokemon.name + "<br>" + pokemon.image + "<br>" + pokemon.type + "<br>");
-           });
-       });
-    });
+$(document).ready(function () {
+  $("button").click(function () {
+    $.getJSON("data/pokedex.json", function (data)
+       {var i = 0;
+       for (var i = 0; i < 151; i++) {
+      $("#PokemonInfo").html("<img src='" + data.pokemon[i].img + "'>" +
+       "<br>" + data.pokemon[i].num + "<br>" +
+       data.pokemon[i].name + "<br>" +
+       data.pokemon[i].type + "<br>");
+
+    }
+
+
+
+  });
+  });
+});
